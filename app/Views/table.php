1158737c48a41eb9db80397bb06e55d9336a8ac8
edipraogo123 <edidoaibu5@gmail.com>
2021-08-18 19:12:41 +0700

    edit

--- app/Views/table.php
+++ app/Views/table.php
@@ -33,14 +33,9 @@
                   <label for="inputPassword">Password</label>
                 </div>
 
-                <div class="custom-control custom-checkbox mb-3">
-                  <input type="checkbox" class="custom-control-input" id="customCheck1">
-                  <label class="custom-control-label" for="customCheck1">Remember password</label>
-                </div>
+               
                 <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
-                <hr class="my-4">
-                <a class="btn btn-lg btn-google btn-block text-uppercase" type="button"><i class="fab fa-google mr-2"></i> Sign in with Google</a>
-                <button class="btn btn-lg btn-facebook btn-block text-uppercase" type="submit"><i class="fab fa-facebook-f mr-2"></i> Sign in with Facebook</button>
+               
               </form>
             </div>
           </div>
@@ -53,8 +48,8 @@
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <!-- <script src="js/jquery-3.4.1.slim.min.js" ></script> -->
-    <!-- <script src="js/popper.min.js" ></script> -->
+    <script src="/js/jquery-3.4.1.slim.min.js" ></script>
+    <script src="/js/popper.min.js" ></script>
     <script src="/js/bootstrap.min.js" ></script>
     <script src="/js/bootstrap.bundle.min.js" ></script>
     <script src="/js/jquery.slim.min.js"></script>
